7f1bc903af579cba135fe069e1c0abc0038d896e
priebebel <37842871+priebebel@users.noreply.github.com>
2021-03-24 21:54:39 -0300

    Add files via upload

---
+++ Atividade5/Media.js
@@ -0,0 +1,6 @@
+var nome = prompt("Qual seu nome? ");
+var notaUm = prompt("Sua primeira nota: ");
+var notaDois = prompt("Sua segunda nota: ");
+var notaTres = prompt("Sua terceira nota: ");
+var media = (parseFloat(notaUm) + parseFloat(notaDois) + parseFloat(notaTres))/3;
+prompt(media);
---
+++ Atividade5/Operacoes.js
@@ -0,0 +1,6 @@
+var numUm = prompt("Digite o numero 1: ");
+var numDois = prompt("Digite o numero 2: ");
+prompt(parseFloat(numUm) + parseFloat(numDois));
+prompt(parseFloat(numUm) - parseFloat(numDois));
+prompt(parseFloat(numUm) / parseFloat(numDois));
+prompt(parseFloat(numUm) % parseFloat(numDois));
